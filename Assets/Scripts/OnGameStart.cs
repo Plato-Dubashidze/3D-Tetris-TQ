198509f8bd221b9cec4c0c91a37553133796baea
Plato Dubashidze <gleb.sochyvets@gmail.com>
2022-11-30 15:09:48 +0200

    Last

---
+++ Assets/Scripts/OnGameStart.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGameStart : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 300;
+    }
+
+}
